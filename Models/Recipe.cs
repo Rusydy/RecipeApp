b7da8ace62368f7e17f378b23134be6e50f80b55
Rusydy <rusydymuhiddin@gmail.com>
2023-09-08 23:12:42 +0700

    initial commit

---
+++ Models/Recipe.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RecipeApp
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        // Add other properties as needed
+    }
+}
